6857400e5c04f4e6006892cadadf9db774dd32f6
GazLamp <richgaz111@hotmail.com>
2021-09-27 15:56:48 -0500

    Quote-Generator

---
+++ script.js
@@ -0,0 +1,41 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
+let apiQuotes = []
+
+// Show New Quote
+function newQuote () {
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+  // Check if Author field is blank and replace with 'Unknown'
+  if (!quote.author) {
+    authorText.textContent = 'Unknown'
+  } else {
+    authorText.textContent = quote.author
+  }
+  // Check Quote length to determine styling
+  if (quote.text.length > 100) {
+    quoteText.classList.add('long-quote')
+  } else {
+    quoteText.classList.remove('long-quote')
+  }
+  quoteText.textContent = quote.text
+}
+// Get quotes from API
+async function getQuotes () {
+  const apiUrl = 'https://type.fit/api/quotes'
+  try {
+    const response = await fetch(apiUrl)
+    apiQuotes = await response.json()
+    newQuote()
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+// Event Listener
+newQuoteBtn.addEventListener('click', newQuote)
+// On Load
+getQuotes()
